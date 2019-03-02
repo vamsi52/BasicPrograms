c1f46e0aa0f14c652bf5792e8884041e41d80abc
vamsi52 <47963947+vamsi52@users.noreply.github.com>
2019-03-02 17:06:03 +0530

    java basics program

---
+++ ABCD.java
@@ -0,0 +1,44 @@
+package com.regnant;
+
+public class ABCD {
+	public static void main(String[] args) {
+		int a = 10,b = 20;
+		int z = a+b;
+		System.out.println(z);
+		
+		int x= 10,y=5;
+		int r = x/ y;
+		int r1 = x%y;
+		System.out.println(r);
+		System.out.println(r1);
+		
+		boolean b1 = x!=y;
+		          y++;
+		          x=x-4;
+		          System.out.println(b1);
+		boolean b2 = x==y;          
+	    System.out.println(b2);
+		boolean b3 = (x<y)||(y<=x);
+		
+		if (b3) {
+			System.out.println(b3);
+		}
+		          
+		else {        		  
+			System.out.println(b3);
+		}
+				
+		
+		
+				
+	
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+}
